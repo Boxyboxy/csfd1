9ee3a14ee63a043ae65834e77eb4b32d0cecac43
boxyboxy <eugenengboxiang@gmail.com>
2022-01-24 15:42:45 +0800

    first angular app!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'd1';
+  constructor(private titleService: Title) {
+    titleService.setTitle('My first Angular');
+  }
 }
